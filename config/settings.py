58c194223e87ad5524c33ba46dd232a8bea39787
Nastya Sol <an5oldatova@ya.ru>
2025-04-15 13:28:16 +0300

    test9

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": 'db',
+        "HOST": os.getenv("DB_HOST"),
         "PORT": int(os.getenv("DB_PORT", "5432")),  # default postgres port
     }
 }
